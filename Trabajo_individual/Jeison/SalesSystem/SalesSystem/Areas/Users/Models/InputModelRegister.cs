71a5f57a9d545caa765138e5c40af6319c2201c0
Jeison <jeison.hernan1101@gmail.com>
2021-10-02 16:04:49 -0500

    MVC Proyecto para practicar

---
+++ Trabajo_individual/Jeison/SalesSystem/SalesSystem/Areas/Users/Models/InputModelRegister.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesSystem.Areas.Users.Models
+{
+    public class InputModelRegister
+    {
+        [Required(ErrorMessage = "El campo nombre es obligatorio")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El campo apellido es obligatorio")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "El campo nid es obligatorio")]
+        public string NID { get; set; }
+
+        [Required(ErrorMessage = "El campo telefono es obligatorio")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{2})\)?[-. ]?([0-9]{2})[-. ]?([0-9]{2})$", ErrorMessage = "El formato telefono no es valido")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "El campo correo electrónico es oblogatorio")]
+        [EmailAddress(ErrorMessage = "El campo correo electrónico no es una dirección válida")]
+        public string Email { get; set; }
+
+        [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "El campo contraseña es obligatorio")]
+        [StringLength(100, ErrorMessage = "El numero de caracteres de {0} debe ser al menos {2}.", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
