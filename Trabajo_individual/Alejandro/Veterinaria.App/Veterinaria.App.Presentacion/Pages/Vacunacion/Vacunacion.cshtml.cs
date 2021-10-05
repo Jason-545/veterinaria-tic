a2ce9dd33d76ad9d93387b4a8be7250034cd4693
Diana <dicaroviv@gmail.com>
2021-10-05 17:49:24 -0500

    vacunacion

--- Trabajo_individual/Alejandro/Veterinaria.App/Veterinaria.App.Presentacion/Pages/Vacunacion/Vacunacion.cshtml.cs
+++ Trabajo_individual/Alejandro/Veterinaria.App/Veterinaria.App.Presentacion/Pages/Vacunacion/Vacunacion.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Veterinaria.App.Presentacion
+namespace Veterinaria.App.Presentacion.Pages
 {
     public class VacunacionModel : PageModel
     {
