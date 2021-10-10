5c69bbd5fccd5cfa47ecbb89bc2649dbb86a087a
alejo <jhonalejandro9801@gmail.com>
2021-10-10 18:08:37 -0500

    v3 alejo

--- Trabajo_individual/Alejandro/v2 alejo/Veterinaria.App/Veterinaria.App.Dominio/Entidades/EntidadVeterinario.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Veterinaria.App.Dominio
-{
-    public class EntidadVeterinario: EntidadPersona{
-        public String Especializacion {get; set;}
-        public String TarjetaProfesional {get; set;}
-    }
-}
