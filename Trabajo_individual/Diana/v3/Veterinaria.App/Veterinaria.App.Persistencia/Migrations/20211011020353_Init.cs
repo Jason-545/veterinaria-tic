633cec26c964ba187c413edc01cabe11fc10e28c
Diana <dicaroviv@gmail.com>
2021-10-11 07:19:13 -0500

    veterinario

---
+++ Trabajo_individual/Diana/v3/Veterinaria.App/Veterinaria.App.Persistencia/Migrations/20211011020353_Init.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Veterinaria.App.Persistencia.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cita",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaRegistroCita = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaAtencionCita = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Triage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    idVeterinario = table.Column<int>(type: "int", nullable: false),
+                    idCliente = table.Column<int>(type: "int", nullable: false),
+                    idMascota = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cita", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HistorialClinico",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaRegistroCitaH = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaAtencionCitaH = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Diagnostico = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    idVeterinario = table.Column<int>(type: "int", nullable: false),
+                    idCliente = table.Column<int>(type: "int", nullable: false),
+                    idMascota = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HistorialClinico", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mascota",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreMascota = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EdadMascota = table.Column<int>(type: "int", nullable: false),
+                    IdPropietario = table.Column<int>(type: "int", nullable: false),
+                    Especie = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    raza = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    peso = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mascota", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TipoDocumento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumeroDocumento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Contraseña = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Especializacion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TarjetaProfesional = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vacuna",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreVacuna = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoVacuna = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaRegistroVacuna = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vacuna", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cita");
+
+            migrationBuilder.DropTable(
+                name: "HistorialClinico");
+
+            migrationBuilder.DropTable(
+                name: "Mascota");
+
+            migrationBuilder.DropTable(
+                name: "Personas");
+
+            migrationBuilder.DropTable(
+                name: "Vacuna");
+        }
+    }
+}
